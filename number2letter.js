48c71fd24bdb7dbc8cedbb75352ad9c2358a934b
Jagdeep Singh <59809683+jagdeepsgill888@users.noreply.github.com>
2025-01-17 01:31:30 +0000

    finish Q2

--- number2letter.js
+++ number2letter.js
@@ -1,12 +1,18 @@
 // This programs *number grade* **km** to a **letter grade**
 
-let grade = 62;
+let grade = 85;
 
 if (grade < 60) {
-    console.log(`${grade}is F`)
+    console.log(`A ${grade} % is F`)
    } else if (grade < 70) {
-     console.log(`${grade}is D`)
-   }
+     console.log(`A ${grade} % is D`)
+   } else if (grade < 80) {
+    console.log(`A ${grade} % is c`)
+  } else if (grade < 90) {
+    console.log(`A ${grade} % is B`)
+  } else if (grade < 100) {
+    console.log(`A ${grade} % is A`)
+  }
 
 
 
