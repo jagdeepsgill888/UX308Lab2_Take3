1ff18026d2a0689618f55d5b8fdef5100f7f5c77
Jagdeep Singh <59809683+jagdeepsgill888@users.noreply.github.com>
2025-01-17 01:44:33 +0000

    done Q3

---
+++ cylinder.js
@@ -0,0 +1,5 @@
+let diameter = 2;
+let height = 4;
+let volume = (Math.PI*diameter**2*height)/4;
+
+console.log(`With a diameter of ${diameter}cm and the height of ${height}cm the volume is ${volume}cm2.`)
