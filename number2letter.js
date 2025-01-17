15ad539b0bd48e2d2e61eabb0f5823cd01a1b27c
Jagdeep Singh <59809683+jagdeepsgill888@users.noreply.github.com>
2025-01-17 01:23:21 +0000

    saving Q2

---
+++ number2letter.js
@@ -0,0 +1,13 @@
+// This programs *number grade* **km** to a **letter grade**
+
+let grade = 62;
+
+if (grade < 60) {
+    console.log(`${grade}is F`)
+   } else if (grade < 70) {
+     console.log(`${grade}is D`)
+   }
+
+
+
+     
