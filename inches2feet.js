d2e30b7f3dcb6b4ae900c25fbf509f30fde8d62b
Jagdeep Singh <59809683+jagdeepsgill888@users.noreply.github.com>
2025-01-17 01:00:38 +0000

    Finished Q1

---
+++ inches2feet.js
@@ -0,0 +1,8 @@
+// This programs *converts* **inches** to **feet**
+
+let inches = 5;
+
+let feet = inches/12;
+
+console.log(`${inches} inches is ${feet} feet`);
+
---
+++ km2miles.js
@@ -0,0 +1,7 @@
+// This programs *converts* **km** to **miles**
+
+let km = 8;
+
+let miles = km*0.6213;
+
+console.log(`${km} km is ${miles} miles`);
